60302e0f20f44f7a07d1a32898be2d658b22cf99
Mario Gutierrez <mfdogutierrez@gmail.com>
2021-01-14 02:42:30 -0500

    update config

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
